6f7736037bc31856dc9b5c883e4efdf8d0fec795
ALostGuardian <98481015+ALostGuardian@users.noreply.github.com>
2022-11-20 13:53:21 -0500

    Cutscene Scripts

---
+++ src/Scripts/Dialogue.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+
+var dialouge_1=[
+	{
+	"Name": "Arcaniea",
+	"Position": "1",
+	"Text": "Ah! You're here",
+	"Choices": ["",""],
+	},
+	{"Name": "MC",
+	"Position": "2",
+	"Text": "Huh? Were you waiting for me Arcaniea?",
+	"Choices": ["",""],
+	},
+	
+	
+]

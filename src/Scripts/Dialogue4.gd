91c73800900cbf78c6ef105a6b7f760c9e014c52
ALostGuardian <98481015+ALostGuardian@users.noreply.github.com>
2022-11-27 14:05:03 -0500

    dialogue scripts

---
+++ src/Scripts/Dialogue4.gd
@@ -0,0 +1,53 @@
+extends Node2D
+
+
+var dialouge_1=[
+		{
+	"Name": "Velouria",
+	"Position": "1",
+	"Text": "Hey MC! You got a second?",
+	"Choices": ["",""],
+	},
+	{"Name": "MC",
+	"Position": "2",
+	"Text": "Oh, Hey Velouria! Yeah Im free right now, whatcha need?",
+	"Choices": ["",""],
+	},
+	{
+	"Name": "Velouria",
+	"Position": "1",
+	"Text": "Haha yknown I really love the way you talk, much better than the stuffy nobles around here.",
+	"Choices": ["",""],
+	},
+	{"Name": "MC",
+	"Position": "2",
+	"Text": "Haha hey not all the nobles are bad Velouria.",
+	"Choices": ["",""],
+	},
+	{
+	"Name": "Velouria",
+	"Position": "1",
+	"Text": "Yeaaah well anyway, I was just wondering if you wanted to do some training with me, you’re pretty strong and Im looking for a challenge.",
+	"Choices": ["",""],
+	},
+	{"Name": "MC",
+	"Position": "2",
+	"Text": "Oh! Yeah sure Velouria I wouldnt mind, want to head to the training ground?",
+	"Choices": ["",""],
+	},
+	{
+	"Name": "Velouria",
+	"Position": "1",
+	"Text": "Yeah no, were heading to a much better training spot.",
+	"Choices": ["",""],
+	},
+	{"Name": "MC",
+	"Position": "2",
+	"Text": "I should probably be worried about this, but Ive already agreed so, lets go",
+	"Choices": ["",""],
+	},
+	
+		
+	
+]
+

d5dfca0b62f5afdff7c82e4530f1ec8e8f9a4a4a
ALostGuardian <98481015+ALostGuardian@users.noreply.github.com>
2022-12-06 01:03:44 -0500

    Add files via upload

---
+++ src/Scripts/Odrahn_1_dialogue.gd
@@ -0,0 +1,81 @@
+extends Node2D
+
+
+var dialouge_1=[
+		{
+	"Name": "MC",
+	"Position": "1",
+	"Text": "Odrahn! Its an emergency!",
+	"Choices": ["",""],
+	},
+	{"Name": "Odrahn",
+	"Position": "2",
+	"Text": "O-oh Hi MC! Wh-ats wrong?!",
+	"Choices": ["",""],
+	},
+		{
+	"Name": "MC",
+	"Position": "1",
+	"Text": "I just found this weird animal on my patrol and it doesn't look like they're doing too hot!",
+	"Choices": ["",""],
+	},
+	{"Name": "Odrahn",
+	"Position": "2",
+	"Text": "Okay! Bring them in lets take a look!",
+	"Choices": ["",""],
+	},
+		{
+	"Name": "MC",
+	"Position": "1",
+	"Text": "Oh Odrahn! Are they going to be okay?",
+	"Choices": ["",""],
+	},
+	{
+	"Name": "Odrahn",
+	"Position": "2",
+	"Text": "I think so… it seems like what you brought in is an owl cat, its a unique breed of feline that was bred with snow owls to make better messenger animals",
+	"Choices": ["",""],
+	},
+		{
+	"Name": "MC",
+	"Position": "1",
+	"Text": "Oh! I wonder what they were doing around here then, and so wounded too",
+	"Choices": ["",""],
+	},
+	{
+	"Name": "Odrahn",
+	"Position": "2",
+	"Text": "Most likely it is because it was being used as a messenger, they usually get hunted won especially during times of war",
+	"Choices": ["",""],
+	},
+		{
+	"Name": "MC",
+	"Position": "1",
+	"Text": "That's so cruel of people! I mean look at how precious they are!",
+	"Choices": ["",""],
+	},
+	{
+	"Name": "Odrahn",
+	"Position": "2",
+	"Text": "I know right! I just can't believe the way that people treat animals in this world sometimes!",
+	"Choices": ["",""],
+	},
+		{
+	"Name": "MC",
+	"Position": "1",
+	"Text": "(I dont think Ive ever seen Odrahn so fired up!) I mean I know you're a druid Odrahn but you really care about animals dont you!",
+	"Choices": ["",""],
+	},
+	{
+	"Name": "Odrahn",
+	"Position": "2",
+	"Text": "Well I mean well my field of study is the health of animals so I would say theyre a passion of mine yeah!",
+	"Choices": ["",""],
+	},
+		{
+	"Name": "MC",
+	"Position": "1",
+	"Text": "Well I mean well my field of study is the health of animals so I would say theyre a passion of mine yeah!",
+	"Choices": ["",""],
+	},
+	]

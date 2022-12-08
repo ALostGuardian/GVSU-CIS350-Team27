1ecc379a96150822ee66b38320e1c6560733386f
Seno900 <delancod@mail.gvsu.edu>
2022-12-07 22:15:44 -0500

    Fixed dependencies and small typo

--- src/Scripts/selectRelationshipScene.gd
+++ src/Scripts/selectRelationshipScene.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _ready():
-	pass # Replace with function body.
+	pass 
 
 
 
@@ -9,134 +9,134 @@ func _ready():
 
 func _on_ArcanleaScene1_button_up():
 	get_tree().change_scene("res://src/Scenes/Arcaniea_Scene_1.tscn")
-	pass # Replace with function body.
+	pass 
 
 
 func _on_ArcanleaScene2_button_up():
 	get_tree().change_scene("res://src/Scenes/Arcaniea_Scene_2.tscn")
-	pass # Replace with function body.
+	pass 
 
 
 func _on_ArcanleaScene3_button_up():
 	get_tree().change_scene("res://src/Scenes/Arcaniea_Scene_3.tscn")
-	pass # Replace with function body.
+	pass 
 
 
 func _on_VelouriaScene1_button_up():
 	get_tree().change_scene("res://src/Scenes/Velouria_Scene_1.tscn")
-	pass # Replace with function body.
+	pass 
 
 
 func _on_VelouriaScene2_button_up():
 	get_tree().change_scene("res://src/Scenes/Velouria_Scene_2.tscn")
-	pass # Replace with function body.
+	pass 
 
 
 func _on_VelouriaScene3_button_up():
 	get_tree().change_scene("res://src/Scenes/Velouria_Scene_3.tscn")
-	pass # Replace with function body.
+	pass 
 
 
 func _on_FaeriethScene1_button_up():
-	#get_tree().change_scene("res://src/Scenes/Faerieth_Scene_1.tscn")
-	pass # Replace with function body.
+	get_tree().change_scene("res://src/Scenes/Faerieth_scene_1.tscn")
+	pass 
 
 
 func _on_FaeriethScene2_button_up():
-	#get_tree().change_scene("res://src/Scenes/Faerieth_Scene_2.tscn")
-	pass # Replace with function body.
+	get_tree().change_scene("res://src/Scenes/Faerieth_scene_2.tscn")
+	pass 
 
 
 func _on_FaeriethScene3_button_up():
-	#get_tree().change_scene("res://src/Scenes/Faerieth_Scene_3.tscn")
-	pass # Replace with function body.
+	get_tree().change_scene("res://src/Scenes/Faerieth_scene_3.tscn")
+	pass 
 
 
 func _on_IncriaScene1_button_up():
-	#get_tree().change_scene("res://src/Scenes/Incria_Scene_1.tscn")
-	pass # Replace with function body.
+	get_tree().change_scene("res://src/Scenes/Incria_Scene_1.tscn")
+	pass 
 
 
 func _on_IncriaScene2_button_up():
-	#get_tree().change_scene("res://src/Scenes/Incria_Scene_2.tscn")
-	pass # Replace with function body.
+	get_tree().change_scene("res://src/Scenes/Incria_Scene_2.tscn")
+	pass 
 
 
 func _on_IncriaScene3_button_up():
-	#get_tree().change_scene("res://src/Scenes/Incria_Scene_3.tscn")
-	pass # Replace with function body.
+	get_tree().change_scene("res://src/Scenes/Incria_Scene_3.tscn")
+	pass 
 
 
 func _on_PetrosScene1_button_up():
-	#get_tree().change_scene("res://src/Scenes/Petros_Scene_1.tscn")
-	pass # Replace with function body.
+	get_tree().change_scene("res://src/Scenes/Petros_Scene_1.tscn")
+	pass 
 
 
 func _on_PetrosScene2_button_up():
-	#get_tree().change_scene("res://src/Scenes/Petros_Scene_2.tscn")
-	pass # Replace with function body.
+	get_tree().change_scene("res://src/Scenes/Petros_scene_2.tscn")
+	pass 
 
 
 func _on_PetrosScene3_button_up():
-	#get_tree().change_scene("res://src/Scenes/Petros_Scene_3.tscn")
-	pass # Replace with function body.
+	get_tree().change_scene("res://src/Scenes/Petros_Scene_3.tscn")
+	pass 
 
 
 func _on_KingsleyScene1_button_up():
-	#get_tree().change_scene("res://src/Scenes/Kingsley_Scene_1.tscn")
-	pass # Replace with function body.
+	get_tree().change_scene("res://src/Scenes/Kingsley_scene_1.tscn")
+	pass 
 
 
 func _on_KingsleyScene3_button_up():
-	#get_tree().change_scene("res://src/Scenes/Kingsley_Scene_3.tscn")
-	pass # Replace with function body.
+	get_tree().change_scene("res://src/Scenes/Kingsley_scene_3.tscn")
+	pass 
 
 
 func _on_KingsleyScene2_button_up():
-	#get_tree().change_scene("res://src/Scenes/Kingsley_Scene_2.tscn")
-	pass # Replace with function body.
+	get_tree().change_scene("res://src/Scenes/Kingsley_scene_2.tscn")
+	pass 
 
 
 func _on_OdrahnScene1_button_up():
-	#get_tree().change_scene("res://src/Scenes/Odrahn_Scene_1.tscn")
-	pass # Replace with function body.
+	get_tree().change_scene("res://src/Scenes/Odrahn_scene_1.tscn")
+	pass 
 
 
 func _on_OdrahnScene2_button_up():
-	#get_tree().change_scene("res://src/Scenes/Odrahn_Scene_2.tscn")
-	pass # Replace with function body.
+	get_tree().change_scene("res://src/Scenes/Odrahn_scene_2.tscn")
+	pass 
 
 
 func _on_OdrahnScene3_button_up():
-	#get_tree().change_scene("res://src/Scenes/Odrahn_Scene_3.tscn")
-	pass # Replace with function body.
+	get_tree().change_scene("res://src/Scenes/Odrahn_scene_3.tscn")
+	pass 
 
 
 func _on_exitButton_button_up():
 	get_tree().quit()
-	pass # Replace with function body.
+	pass 
 
 
 func _on_mainMenuButton_button_up():
 	get_tree().change_scene("res://src/Scenes/mainMenu.tscn")
-	pass # Replace with function body.
+	pass 
 
 
 func _on_oobButton_button_up():
 	get_tree().change_scene("res://src/Scenes/outOfBattleMenu.tscn")
-	pass # Replace with function body.
+	pass 
 
 
 func _on_AshtonScene1_button_up():
-	#get_tree().change_scene("res://src/Scenes/Ashton_Scene_1.tscn")
-	pass # Replace with function body.
+	get_tree().change_scene("res://src/Scenes/Ashton_Scene_1.tscn")
+	pass 
 
 
 func _on_AshtonScene2_button_up():
-	#get_tree().change_scene("res://src/Scenes/Ashton_Scene_2.tscn")
-	pass # Replace with function body.
+	get_tree().change_scene("res://src/Scenes/Ashton_Scene_2.tscn")
+	pass 
 
 
 func _on_AshtonScene3_button_up():
-	#get_tree().change_scene("res://src/Scenes/Ashton_Scene_3.tscn")
-	pass # Replace with function body.
+	get_tree().change_scene("res://src/Scenes/Ashton_Scene_3.tscn")
+	pass 

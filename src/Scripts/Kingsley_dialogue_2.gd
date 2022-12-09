b557cf9ee64eba9a66dead01a0d73b783dfcdc7b
ALostGuardian <98481015+ALostGuardian@users.noreply.github.com>
2022-12-08 22:59:21 -0500

    Add files via upload

---
+++ src/Scripts/Kingsley_dialogue_2.gd
@@ -0,0 +1,124 @@
+extends Node2D
+
+
+var dialouge_1=[
+	
+	{"Name": "Kingsley",
+	"Position": "2",
+	"Text": "Good morning My Lady!",
+	"Choices": ["",""],
+	},
+	{
+	"Name": "MC",
+	"Position": "1",
+	"Text": "whihahbuio;i/ what?!",
+	"Choices": ["",""],
+	},
+	{"Name": "Kingsley",
+	"Position": "2",
+	"Text": "I have come here to retrieve you for morning training!",
+	"Choices": ["",""],
+	},
+	{
+	"Name": "MC",
+	"Position": "1",
+	"Text": "Kingsley why are you yelling?!",
+	"Choices": ["",""],
+	},
+	{"Name": "Kingsley",
+	"Position": "2",
+	"Text": "Well I cannot enter your tent and you weren't waiting up with my normal talking my lady!",
+	"Choices": ["",""],
+	},
+	{
+	"Name": "MC",
+	"Position": "1",
+	"Text": "Ugh… Okay gimme a minute let me get ready",
+	"Choices": ["",""],
+	},
+	{
+	"Name": "Kingsley",
+	"Position": "1",
+	"Text": "Alright, are you ready to be off now?",
+	"Choices": ["",""],
+	},
+	{
+	"Name": "MC",
+	"Position": "1",
+	"Text": "*Yawn* as ready as Ill ever be I guess",
+	"Choices": ["",""],
+	},
+	{
+	"Name": "Kingsley",
+	"Position": "1",
+	"Text": "Good let us begin with warm ups, then we do some sparring, after that we will have breakfast with the rest of the camp. We will then do the morning rounds to guarantee that everything is in the palace. After that we will…",
+	"Choices": ["",""],
+	},
+	{
+	"Name": "MC",
+	"Position": "1",
+	"Text": "Wow thats a lot I already feel tired",
+	"Choices": ["",""],
+	},
+	{
+	"Name": "Kingsley",
+	"Position": "1",
+	"Text": "It can be a lot to adjust to, please let me know if it too much, I have accounted for break times in todays schedule for you to rest. We can take a breather over by the lake you always seem to scout by.",
+	"Choices": ["",""],
+	},
+		{
+	"Name": "MC",
+	"Position": "1",
+	"Text": "Oh you noticed that? The palace lake is just especially clear",
+	"Choices": ["",""],
+	},
+	{
+	"Name": "Kingsley",
+	"Position": "1",
+	"Text": "Ah yes I believe that is because of the special type of magic that the water users cast upon it…",
+	"Choices": ["",""],
+	},
+	{
+	"Name": "MC",
+	"Position": "1",
+	"Text": "(Kingsley talks a lot more than I thought he did)",
+	"Choices": ["",""],
+	},
+		{
+	"Name": "Kingsley",
+	"Position": "1",
+	"Text": "Oh I apologize it seems I am boring you, I will stop my tangent now",
+	"Choices": ["",""],
+	},
+		{
+	"Name": "MC",
+	"Position": "1",
+	"Text": "Oh no I'm sorry Im just tired I wasn't trying to ignore you please keep talking!",
+	"Choices": ["",""],
+	},
+		{
+	"Name": "Kingsley",
+	"Position": "1",
+	"Text": "Would you like me to continue talking?",
+	"Choices": ["",""],
+	},
+	{
+	"Name": "MC",
+	"Position": "1",
+	"Text": "Well yeah of course!",
+	"Choices": ["",""],
+	},
+	{
+	"Name": "Kingsley",
+	"Position": "1",
+	"Text": "Oh, I apologize, I am not as knowledgeable about social interactions, and as such I am very aware of everything I say.",
+	"Choices": ["",""],
+	},
+	{
+	"Name": "MC",
+	"Position": "1",
+	"Text": "Haha now I get why you keep going on tangents about stuff I like, stop worrying Kingsley I like talking to you just the way you are",
+	"Choices": ["",""],
+	},
+	
+	]
---
+++ src/Scripts/Kingsley_dialogue_3.gd
@@ -0,0 +1,113 @@
+extends Node2D
+
+
+var dialouge_1=[
+
+	{
+	"Name": "MC",
+	"Position": "1",
+	"Text": "Ah Im up pretty early Kingsley isn't even here to get me yet",
+	"Choices": ["",""],
+	},
+	{"Name": "Kingsley",
+	"Position": "2",
+	"Text": "Good morning MC I am here to retrieve you for training",
+	"Choices": ["",""],
+	},
+	{
+	"Name": "MC",
+	"Position": "1",
+	"Text": "Oh Kingsley it's a little early, why don't you come inside and we can have some tea before we go?",
+	"Choices": ["",""],
+	},
+	{"Name": "Kingsley",
+	"Position": "2",
+	"Text": "Uh I think I will stay out here",
+	"Choices": ["",""],
+	},
+	{
+	"Name": "MC",
+	"Position": "1",
+	"Text": "Uh why?",
+	"Choices": ["",""],
+	},
+	{
+	"Name": "Kingsley",
+	"Position": "1",
+	"Text": "Um well, its against my oath to enter another's chambers while they are alone",
+	"Choices": ["",""],
+	},
+	{
+	"Name": "MC",
+	"Position": "1",
+	"Text": "OH! But I don't understand why not? Im inviting you in",
+	"Choices": ["",""],
+	},
+	{
+	"Name": "Kingsley",
+	"Position": "1",
+	"Text": "We take an oath like this to dedicate ourselves to the one we shall marry one day, we vow to only be alone in their presents within private areas such as the home.",
+	"Choices": ["",""],
+	},
+	{
+	"Name": "MC",
+	"Position": "1",
+	"Text": "Oh well what about close friends? Can you be alone with them?",
+	"Choices": ["",""],
+	},
+	{
+	"Name": "Kingsley",
+	"Position": "1",
+	"Text": "The oath is quite certain about its terms.",
+	"Choices": ["",""],
+	},
+		{
+	"Name": "MC",
+	"Position": "1",
+	"Text": "Just come inside Kingsley it's early and cold!",
+	"Choices": ["",""],
+	},
+	{
+	"Name": "Kingsley",
+	"Position": "1",
+	"Text": "Well, you have a lot of decorations in such a small space",
+	"Choices": ["",""],
+	},
+	{
+	"Name": "MC",
+	"Position": "1",
+	"Text": "Yeah its all the nicknacks I've gathered over the years, here some tea!",
+	"Choices": ["",""],
+	},
+		{
+	"Name": "Kingsley",
+	"Position": "1",
+	"Text": "Thank you",
+	"Choices": ["",""],
+	},
+		{
+	"Name": "MC",
+	"Position": "1",
+	"Text": "Well it looks were cheating on your vow a little bit so I guess this makes us pretty close friends",
+	"Choices": ["",""],
+	},
+		{
+	"Name": "Kingsley",
+	"Position": "1",
+	"Text": "Or more",
+	"Choices": ["",""],
+	},
+	{
+	"Name": "MC",
+	"Position": "1",
+	"Text": "What did you say?",
+	"Choices": ["",""],
+	},
+	{
+	"Name": "Kingsley",
+	"Position": "1",
+	"Text": "Nothing",
+	"Choices": ["",""],
+	},
+	
+	]
